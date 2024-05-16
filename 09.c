092d5ceecac65569260f32c5795c91bf30f0ca29
Anna Rocha <168745681+Annacrocha@users.noreply.github.com>
2024-05-16 09:30:05 -0300

    Create 09.c

---
+++ 09.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+	
+	float valor_Raio,area,comprimento,diametro;
+	
+	printf("Considere pi 3,14 \n");
+	printf("Digite o valor do raio de uma circunferencia: ");
+	scanf("%f", &valor_Raio);
+ 
+  		diametro = 2 * valor_Raio;
+  		comprimento = (2 * 3,14) * valor_Raio;
+  		area = 3,14 *( valor_Raio*valor_Raio);
+	
+			printf("Diametro: %f \n", diametro);
+			printf("Comprimento: %f \n", comprimento);
+			printf("Area: %f \n", area);
+	
+}
