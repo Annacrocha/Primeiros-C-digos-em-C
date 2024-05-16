5f9b560ad1558d73895dbe3316d151d53daedba0
Anna Rocha <168745681+Annacrocha@users.noreply.github.com>
2024-05-16 08:06:03 -0300

    Create 06.c

---
+++ 06.c
@@ -0,0 +1,16 @@
+include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int peso_Quilos;
+	
+	printf("Digite seu peso (em quilos): ");
+	scanf("%i", &peso_Quilos);
+	
+	int peso_Gramas = peso_Quilos * 1000;
+	
+	printf("Peso em gramas: %i \n", peso_Gramas);
+	
+	return 0;
+}
